fde8fc370b7f7f005269212b7e32f423c4389f4e
Khusela Mphokeli <innothetechgeek@gmail.com>
2022-06-21 11:53:08 +0200

    minor change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Test Application - Minor change from localhost
                 </div>
 
                 <div class="links">
